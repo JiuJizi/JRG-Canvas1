b68e4afdc381cfe63eee6e26b6836adf484f800c
jiujizi <3505894611@qq.com>
2019-02-04 09:37:35 +0800

    触屏设备兼容

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
     
     setCanvasSize(yyy)
 
-    listenToMouse(yyy)
+    listenToUser(yyy)
 
     var eraserEnabled = false
     eraser.onclick = function(){
@@ -30,45 +30,89 @@
         }
     }
     
-    function listenToMouse(canvas){
+    function listenToUser(canvas){
         var context = canvas.getContext('2d'); //获取2d的上下文
         var using = false //绘画启动开关
         var lastPoint = {x:undefined,y:undefined}//最后的一个点
-        canvas.onmousedown = function(aaa){
-        var x = aaa.clientX
-        var y = aaa.clientY
-        using = true
-        if(eraserEnabled){//如果使用橡皮擦时，就执行擦除
-            context.clearRect(x-5,y-5,10,10)
-        }else{//如果没使用橡皮擦，就正常绘图
-            lastPoint = {"x":x,"y":y}
-            }
-        }
-        canvas.onmousemove = function(aaa){
+        
+        //特性检测，不针对设备
+        if(document.body.ontouchstart !== undefined){//检测屏幕是否支持touch(触摸)事件，支持则执行下面的代码
+            //触屏设备
+            canvas.ontouchstart = function(bbb){
+                var x = bbb.touches[0].clientX
+                var y = bbb.touches[0].clientY
+                using = true
+                if(eraserEnabled){//如果使用橡皮擦时，就执行擦除
+                    context.clearRect(x-5,y-5,10,10)
+                }else{//如果没使用橡皮擦，就正常绘图
+                    lastPoint = {"x":x,"y":y}
+                    }
+                }
+                canvas.ontouchmove = function(bbb){
+                    var x = bbb.touches[0].clientX
+                    var y = bbb.touches[0].clientY
+                    if(!using){return}
+                    if(eraserEnabled){
+                        context.clearRect(x-5,y-5,10,10)}
+                    else{
+                        var newPoint = {"x":x,"y":y}
+                        drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+                        lastPoint = newPoint             
+                    }
+                }
+                canvas.ontouchend = function(bbb){
+                        using = false
+                        var x2 = bbb.touches[0].clientX
+                        var y2 = bbb.touches[0].clientY
+                        drawLine(x2,y2)
+                }
+                function drawLine(x1,y1,x2,y2){
+                    context.beginPath();//开始绘画
+                    context.moveTo(x1,y1) //开始坐标
+                    context.lineWidth = 5 //线的宽度
+                    context.strokeStyle = 'black'
+                    context.lineTo(x2,y2) //结束坐标
+                    context.stroke() //边框填充
+                    context.closePath() //关闭绘画
+                }
+        }else{
+            //非触屏设备
+            canvas.onmousedown = function(aaa){
             var x = aaa.clientX
             var y = aaa.clientY
-            if(!using){return}
-            if(eraserEnabled){
-                context.clearRect(x-5,y-5,10,10)}
-            else{
-                var newPoint = {"x":x,"y":y}
-                drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
-                lastPoint = newPoint             
+            using = true
+            if(eraserEnabled){//如果使用橡皮擦时，就执行擦除
+                context.clearRect(x-5,y-5,10,10)
+            }else{//如果没使用橡皮擦，就正常绘图
+                lastPoint = {"x":x,"y":y}
+                }
             }
-        }
-        canvas.onmouseup = function(aaa){
-                using = false
-                var x2 = aaa.clientX
-                var y2 = aaa.clientY
-                drawLine(x2,y2)
-        }
-        function drawLine(x1,y1,x2,y2){
-            context.beginPath();//开始绘画
-            context.moveTo(x1,y1) //开始坐标
-            context.lineWidth = 5 //线的宽度
-            context.strokeStyle = 'black'
-            context.lineTo(x2,y2) //结束坐标
-            context.stroke() //边框填充
-            context.closePath() //关闭绘画
+            canvas.onmousemove = function(aaa){
+                var x = aaa.clientX
+                var y = aaa.clientY
+                if(!using){return}
+                if(eraserEnabled){
+                    context.clearRect(x-5,y-5,10,10)}
+                else{
+                    var newPoint = {"x":x,"y":y}
+                    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+                    lastPoint = newPoint             
+                }
+            }
+            canvas.onmouseup = function(aaa){
+                    using = false
+                    var x2 = aaa.clientX
+                    var y2 = aaa.clientY
+                    drawLine(x2,y2)
+            }
+            function drawLine(x1,y1,x2,y2){
+                context.beginPath();//开始绘画
+                context.moveTo(x1,y1) //开始坐标
+                context.lineWidth = 5 //线的宽度
+                context.strokeStyle = 'black'
+                context.lineTo(x2,y2) //结束坐标
+                context.stroke() //边框填充
+                context.closePath() //关闭绘画
+            }
         }
     }
